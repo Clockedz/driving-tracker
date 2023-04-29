527ea5d596185104693aca85248756309a1490bb
Clockedz <epixplayz222@gmail.com>
2023-04-29 16:13:31 -0700

    changed timezone

--- src/pages/api/serverTime.ts
+++ src/pages/api/serverTime.ts
@@ -1,9 +1,10 @@
 import {NextApiRequest, NextApiResponse} from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	const options = {timeZone: "America/Los_Angeles"};
 	const data = {
-		time: new Date().toLocaleTimeString(),
-		date: new Date().toLocaleDateString(),
+		time: new Date().toLocaleTimeString("en-US", options),
+		date: new Date().toLocaleDateString("en-US", options),
 		total: new Date(),
 	};
 	res.status(200).json(data);
