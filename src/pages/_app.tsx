d13da290a7317ff9cbec2483a612d66eda91f92b
Clockedz <epixplayz222@gmail.com>
2023-05-15 09:52:35 -0700

    updated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../../public/fonts/font.css";
+import {Analytics} from "@vercel/analytics/react";
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
 		<main className="">
 			<Component {...pageProps} />
+			<Analytics />
 		</main>
 	);
 }
